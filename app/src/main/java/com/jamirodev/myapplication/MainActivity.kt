bee5fb1672d04845720db6815cd5369a3ef94b1f
Jamiro <jamirocristian15@gmail.com>
2023-07-18 14:18:24 -0400

    exercise num 2. Learning Compose with Kotlin. Done

---
+++ app/src/main/java/com/jamirodev/myapplication/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.jamirodev.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jamirodev.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TasksCompleted()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TasksCompleted(){
+    TaskCompose(
+        title = "All task completed",
+        subtitle = "Nice Work!",
+        imagePainter = painterResource(R.drawable.ic_task_completed)
+    )
+}
+
+@Composable
+private fun TaskCompose(
+    title: String,
+    subtitle: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = imagePainter, contentDescription = null )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = subtitle,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TasksPreview() {
+    MyApplicationTheme {
+        TasksCompleted()
+    }
+}
